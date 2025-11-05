85a37d2a7ad7fb73e738c2f3adf8f37ed17ebcab
sdas0807 <saptarshidasaws@gmail.com>
2025-11-05 05:02:23 +0000

    changed ec2 tags and added manual dependency

--- ec2.tf
+++ ec2.tf
@@ -3,11 +3,14 @@ resource "aws_instance" "example" {
   ami           = "ami-0a25a306450a2cba3"
   instance_type = "t2.nano"
   key_name      = "roche-key"
-  tags_all = {
+  # changing tags_all to tags
+  tags = {
     "Name" = "sdas-vm"
   }
 }
 resource "local_file" "ip_file" {
     content  = aws_instance.example.public_ip
     filename = "ec2_public_ip.txt"
+    #making manual dependency
+    depends_on = [ aws_instance.example ]
 }
